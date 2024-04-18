df043480312cf1dba47b5379b1544c19908e284f
Sakthi Prakash K <92044150+SAKTHIPRAKASH28@users.noreply.github.com>
2024-04-18 10:54:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,20 @@ from streamlit_option_menu import option_menu
 import predictPage
 import about
 st.set_page_config(layout="wide")
-
+st.markdown(
+    """
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <script>
+    <head><script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1000196417768821" crossorigin="anonymous"></script> </head>
+    
+      (adsbygoogle = window.adsbygoogle || []).push({
+        google_ad_client: "ca-pub-1000196417768821",
+        enable_page_level_ads: true
+      });
+    </script>
+    """,
+    unsafe_allow_html=True
+)
 with st.sidebar:
     selected = option_menu(None, ["Predict", 'About'],
                            icons=['chat', 'info-circle'], menu_icon="list", default_index=0)
